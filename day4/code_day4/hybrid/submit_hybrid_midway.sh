2f5c5b19d93038091c8377596dbbd5c753e05a5c
rccguest 8457 <rccguest8457@midway-login1.rcc.local>
2018-07-19 22:34:04 -0500

    transfer

--- day4/code_day4/hybrid/submit_hybrid_midway.sh
+++ day4/code_day4/hybrid/submit_hybrid_midway.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# a sample job submission script to submit a hybrid MPI/OpenMP job to the sandyb 
-# partition on Midway1 please change the --partition option if you want to use 
+# a sample job submission script to submit a hybrid MPI/OpenMP job to the sandyb
+# partition on Midway1 please change the --partition option if you want to use
 # another partition on Midway1
 
 # set the job name to hello-hybrid
